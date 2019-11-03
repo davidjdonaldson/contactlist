ad3ee1c281c257e60214d9df8dc7fc4343b5075f
unknown <david@donaldsons.nz>
2019-11-03 10:02:55 -0700

    Cleaned up the validation methods and fixed some bugs.

--- view/js/script.js
+++ view/js/script.js
@@ -1,53 +1,49 @@
 // JS code goes here
-var firstName = document.getElementById('name').value;
-var mobile = document.getElementById('mobile').value;
-var email = document.getElementById('email').value;
 var nameRegex = new RegExp('[A-Za-z_]');
 var mobileRegex = new RegExp('^[0-9]{1,10}$');
 var emailRegex = new RegExp('@');
 
-
-
-
 document.getElementById("submit").addEventListener("click", validateInputs);
 document.getElementById("nameColumn").addEventListener("click", sort);
 
-
 function validateInputs() {
     var errorDisplay = document.getElementById('error');
-
+    
     if (validateName() === false || validateMobile() === false || validateEmail() === false) {
-        disp.setAttribute('style', 'display:block;');
+        errorDisplay.setAttribute('style', 'display:block;');
     } else {
         addContact();
         document.getElementById("name").value = '';
         document.getElementById("mobile").value = '';
         document.getElementById("email").value = '';
-        disp.setAttribute('style', 'display:hidden;');
+        errorDisplay.setAttribute('style', 'display:hidden;');
         console.log("success");
     }
 
 }
 
 function validateName() {
- if(nameRegex.test(firstName) === false)
-    return false;
-else
+var firstName = document.getElementById('name').value;
+ if(nameRegex.test(firstName) === true)
     return true;
+else
+    return false;
 }
 
 function validateMobile() {
-     if(mobileRegex.test(mobile) === false)
-    return false;
-else
+var mobile = document.getElementById('mobile').value;
+if(mobileRegex.test(mobile) === true)
     return true;
+else
+    return false;
 }
 
 function validateEmail() {
-     if(emailRegex.test(email) === false)
-    return false;
-else
+var email = document.getElementById('email').value;
+if(emailRegex.test(email) === true)
     return true;
+else
+    return false;
 }
 
 function addContact() {
@@ -57,13 +53,13 @@ function addContact() {
 
     var table = document.getElementById("summaryTable");
     var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
+    var nameCell = row.insertCell(0);
+    var mobileCell = row.insertCell(1);
+    var emailCell = row.insertCell(2);
 
-    cell1.innerHTML = sessionStorage.Name;
-    cell2.innerHTML = sessionStorage.Mobile;
-    cell3.innerHTML = sessionStorage.Email;
+    nameCell.innerHTML = sessionStorage.Name;
+    mobileCell.innerHTML = sessionStorage.Mobile;
+    emailCell.innerHTML = sessionStorage.Email;
 };
 
 function sort() {
