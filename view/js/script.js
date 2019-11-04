ea8ebd9b3313f13ef7302ee24cd31a3c002ece97
unknown <david@donaldsons.nz>
2019-11-03 21:16:43 -0700

    Updated save addContact, working on render

--- view/js/script.js
+++ view/js/script.js
@@ -1,13 +1,13 @@
 const nameRegex = new RegExp('[A-Za-z_]');
 const mobileRegex = new RegExp('^[0-9]{1,10}$');
 const emailRegex = new RegExp('@');
-var errorDisplay = document.getElementById('error');
+const errorDisplay = document.getElementById('error');
 var table = document.getElementById("summaryTable");
 var row = table.insertRow(1);
 var nameCell = row.insertCell(0);
 var mobileCell = row.insertCell(1);
 var emailCell = row.insertCell(2);
-var getContacts 
+var allContacts = []
 
 document.getElementById("submit").addEventListener("click", validateInputs);
 document.getElementById("nameColumn").addEventListener("click", sort);
@@ -17,54 +17,77 @@ function validateInputs() {
     if (validateName() === false || validateMobile() === false || validateEmail() === false) {
         errorDisplay.setAttribute('style', 'display:block!important;');
     } else {
-        storeValues();
+        addContact();
         clearInputs();
+        render();
         errorDisplay.setAttribute('style', 'display:hidden!important;');
     }
 }
 
 function validateName() {
-var firstName = document.getElementById('name').value;
+const firstName = document.getElementById('name').value;
  if(nameRegex.test(firstName) === true)
     return true;
 return false;
 }
 
 function validateMobile() {
-var mobile = document.getElementById('mobile').value;
+const mobile = document.getElementById('mobile').value;
 if(mobileRegex.test(mobile) === true)
     return true;
 return false;
 }
 
 function validateEmail() {
-var email = document.getElementById('email').value;
+const email = document.getElementById('email').value;
 if(emailRegex.test(email) === true)
     return true;
 return false;
 }
 
-function storeValues() {
-    var contactList = {
-    name: document.getElementById('name').value,
-    mobile: document.getElementById('mobile').value,
-    email: document.getElementById('email').value
-}
-    window.localStorage.setItem('contacts', JSON.stringify(contactList));
-    renderHTML();
+function addContact() {
+    var existingContacts = JSON.parse(localStorage.getItem('allContacts'));
+    if(existingContacts == null) 
+        existingContacts = [];
+    var name = document.getElementById('name').value;
+    var mobile = document.getElementById('mobile').value;
+    var email = document.getElementById('email').value;
+    var contact = {
+        "name": name,
+        "mobile": mobile,
+        "email": email
+    };
+    localStorage.setItem("contact", JSON.stringify(contact));
+    // Append existing entries back
+    allContacts.push(contact);
+    localStorage.setItem("existingContacts", JSON.stringify(allContacts));
 }
 
-function renderHTML() {
-    getContacts = JSON.parse(localStorage.getItem('contacts'));
-    table = document.getElementById("summaryTable");
-    row = table.insertRow(1);
-    nameCell = row.insertCell(0);
-    mobileCell = row.insertCell(1);
-    emailCell = row.insertCell(2);
+function render() {
+    var getContacts = JSON.parse(localStorage.getItem('allContacts'));
+    var input = '';
+    for (var i = 0; i < getContacts.length; i++) {
+           console.log(getContacts);
+           input = '<tr><th>' + getContacts.name + '</th><th>' + getContacts.mobile + '</th><th>' + getContacts.email + '</th></tr>';
+       }
 
-    nameCell.textContent = getContacts.name;
-    mobileCell.textContent = getContacts.mobile;
-    emailCell.textContent = getContacts.email;
+console.log(input);
+
+
+    // table = document.getElementById("summaryTable");
+    // row = table.insertRow(1);
+    // nameCell = row.insertCell(0);
+    // mobileCell = row.insertCell(1);
+    // emailCell = row.insertCell(2);
+
+    // nameCell.textContent = getContacts.name;
+    // mobileCell.textContent = getContacts.mobile;
+    // emailCell.textContent = getContacts.email;
+
+    // const function getContacts() {
+    //     for (var i = 0; i < localStorage.length i++) {
+    //         console.log(localStorage)
+    // }
 };
 
 function clearInputs() {
@@ -99,5 +122,5 @@ inSort.sort(function(a, b) {
   return 0;
 });
 window.localStorage.setItem('contacts', JSON.stringify(inSort));
-renderHTML();
+render();
 }
